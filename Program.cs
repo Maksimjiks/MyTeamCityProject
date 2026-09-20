81f1f30ebff2b30447217568dd8602b82363856e
Maksym <saposnikmaks@gmail.com>
2026-09-20 23:08:17 +0300

    Update application message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyTeamCityProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from TeamCity CI/CD!");
+        }
+    }
+}
