4933812e9242a0aff542c371636118d90fe13bab
Maksym <saposnikmaks@gmail.com>
2026-09-21 00:27:04 +0300

    Fix Program.cs build error

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ namespace MyTeamCityProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from TeamCity CI/CD!");
+            Console.WriteLine("Hello, TeamCity! Automatic build test");
         }
     }
 }
-Console.WriteLine("Hello, TeamCity! Automatic build test");
-Console.WriteLine("Hello, TeamCity! Test 2");
