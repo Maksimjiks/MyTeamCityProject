d03f5eeea7ef1b197141f996a412151483ca6558
Maksym <saposnikmaks@gmail.com>
2026-09-21 08:29:23 +0300

    Test automatic VCS trigger

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyTeamCityProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Automatic VCS trigger test"); 
+            Console.WriteLine("Automatic VCS trigger test1.1"); 
         }
     }
 }
