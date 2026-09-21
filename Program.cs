2e97db16f660e4bd10f4962f3a10ae484490071d
Maksym <saposnikmaks@gmail.com>
2026-09-21 13:49:13 +0300

    Test VCS trigger 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyTeamCityProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Automatic VCS trigger test1.1"); 
+            Console.WriteLine("VCS Trigger test 2");
         }
     }
 }
